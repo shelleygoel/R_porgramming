9af796291a1d5d141380ce2c68b509cad007106b
shelley goel <shelley.goel109@gmail.com>
2016-02-27 17:58:47 -0800

    ranking hospital function complete

---
+++ ProgrammingAssignment3/pa3_data/rankhospital.R
@@ -0,0 +1,47 @@
+rankhospital <- function(state,outcome,num="best"){
+         
+        data <- read.csv("outcome-of-care-measures.csv",
+                         na.strings="Not Available",stringsAsFactors = F)
+        
+        # column nos in data frame data which contain values of outcomes in consideration
+        outcomes <- c("Heart Attack" = 11, "Heart Failure" = 17, "Pneumonia" = 23 )
+        
+        # renaming required columns of data to more simpler names
+        for (a in names(outcomes)){
+                names(data)[outcomes[[a]]] <- gsub(" ",".",a)
+        }
+        
+        # incorrect state
+        if (!(toupper(state) %in% toupper(data$State))){
+                stop("invalid state")
+        }
+        
+        # incorrect outcome
+        if (!(tolower(outcome) %in% tolower(names(outcomes))) ){
+                stop("invalid outcome")
+        }
+        
+        # converting outcome to corresponding column name
+        outcome <- gsub(" ",".",simpleCap(outcome))
+        
+        # Reducing data to three columns: State,Hospital name, outcome 
+        red_data <- subset(data, State == state, 
+                           select = c("Hospital.Name", "State", outcome))
+        
+        red_data <- na.omit(red_data)
+        
+        if (num == "best") num <- 1
+        if (is.integer(num) & num > nrow(red_data)) return(NA)
+        
+        # row index at which has minimum deaths
+        
+        sort_seq <- order(red_data[[outcome]],red_data$Hospital.Name)
+        
+        if (num == "worst") 
+                best_hosp <- red_data$Hospital.Name[tail(sort_seq,n=1)]
+        
+        else
+                best_hosp <- red_data$Hospital.Name[sort_seq[num]]
+        
+        best_hosp  # hospital with least number of deaths for outcome
+}
